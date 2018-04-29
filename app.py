f928ffa5dd9a66ff1eb055e07d45a6f674880e6e
mayank.agarwal@students.iiit.ac.in <mayank.agarwal@students.iiit.ac.in>
2018-04-29 15:09:56 +0530

    my second last commit

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 import sqlite3
 import bcrypt
 import test
+import time
+import datetime
 
 emailg = "a"
 login = False
@@ -89,6 +91,13 @@ def add():
         sd = request.form['sdate']
         ed = request.form['edate']
         kk = request.form['typee']
+        col = request.form['colora']
+        if col == "1":
+            colo = "red"
+        elif col == "2":
+            colo = "green"
+        else:
+            colo = "white"
         rowsq = None
         cur.execute('SELECT EMAIL FROM users where TASK = ?', (taska,))
         rowsq = cur.fetchall()
@@ -99,8 +108,8 @@ def add():
             if row[0] == emailg:
                 flag = True
         if flag is False:
-            cur.execute("INSERT INTO users (EMAIL,TASK,STARTING,ENDING,STATUS) VALUES (?,?,?,?,?);",
-                        (emailg, taska, sd, ed, kk))
+            cur.execute("INSERT INTO users (EMAIL,TASK,STARTING,ENDING,STATUS,COLOR) VALUES (?,?,?,?,?,?);",
+                        (emailg, taska, sd, ed, kk,colo))
             conn.commit()
         cur.execute('SELECT * FROM users where EMAIL = ?', (emailg,))
         row = cur.fetchall()
@@ -147,6 +156,50 @@ def dashboard():
     return redirect('login.html')
 
 
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+        conn = sqlite3.connect('db/test.db')
+        cur = conn.cursor()
+        se = request.form['searching']
+        option = request.form['sea']
+        print(se)
+        if option is "1":
+            cur.execute('SELECT * FROM users where STATUS = ? and EMAIL= ?', (se,emailg,))
+        else:
+            cur.execute('SELECT * FROM users where TASK = ? and EMAIL = ? ', (se,emailg))
+        row = cur.fetchall()
+        conn.close()
+        return render_template('dashboard.html', data=row)
+
+
 if __name__ == "__main__":
+    conn = sqlite3.connect('db/test.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM users')
+    row = cur.fetchall()
+    for x in row:
+        newdate1 = time.strptime(str(x[3]), "%Y-%m-%d")
+        newdate2 = time.strptime(str(x[4]), "%Y-%m-%d")
+        now = datetime.date.today()
+        now = str(now)
+        # print(now)
+        date = time.strptime(now, "%Y-%m-%d")
+        # print(date)
+        if(newdate1 < date):
+            cur.execute(
+                'UPDATE users SET STATUS = "TO DO" where EMAIL = ?', (x[1],))
+            conn.commit()
+            pass
+        if newdate1 >= date and newdate2 <= date:
+            cur.execute(
+                'UPDATE users SET STATUS = "IN PROGRESS" where EMAIL = ?', (x[1],))
+            conn.commit()
+        if newdate2 > date:
+            cur.execute(
+                'UPDATE users SET STATUS="DONE" where EMAIL = ?', (x[1],))
+            conn.commit()
+            # print(x[3], x[4] ,now)
+    conn.close()
     app.debug = True
     app.run()
